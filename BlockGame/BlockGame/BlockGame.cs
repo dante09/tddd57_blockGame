a72004beb7d0bd7689baf0a44c3814af564e7454
Daniel Andersson <danan612@student.liu.se>
2013-02-13 10:48:49 +0100

    initial project

---
+++ BlockGame/BlockGame/BlockGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlockGame
+{
+    class BlockProgram
+    {
+
+        public static void Main()
+        {
+            using (MainGame game = new MainGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
